4d98ae5454439515f1d6c2f5904788d9c39761c2
Onno Broekmans <o.broekmans@lumicks.com>
2017-07-28 15:37:51 +0200

    Update version in setup.py to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qtsass',
-    version='0.1',
+    version='0.3',
     description='Compile an SCSS/SASS stylesheet file to valid Qt CSS',
 
     packages=find_packages(),
